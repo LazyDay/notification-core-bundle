6cc53435e04dec1788b3adfdc73c8101d302d8ad
Pavel Stepanets <pahhan.ne@gmail.com>
2017-04-24 18:35:16 +0700

    fix swift message typy

--- Driver/SwiftMailerMessage.php
+++ Driver/SwiftMailerMessage.php
@@ -8,26 +8,25 @@ namespace SymfonyBro\NotificationCoreBundle\Driver;
 
 
 use SymfonyBro\NotificationCore\Model\AbstractMessage;
-use SymfonyBro\NotificationCore\Model\MessageInterface;
 use SymfonyBro\NotificationCore\Model\NotificationInterface;
 
 class SwiftMailerMessage extends AbstractMessage
 {
     /**
-     * @var \Swift_Message
+     * @var \Swift_Mime_Message
      */
     private $swiftMessage;
 
-    public function __construct(NotificationInterface $notification, MessageInterface $swiftMessage)
+    public function __construct(NotificationInterface $notification, \Swift_Mime_Message $swiftMessage)
     {
         parent::__construct($notification);
         $this->swiftMessage = $swiftMessage;
     }
 
     /**
-     * @return \Swift_Message
+     * @return \Swift_Mime_Message
      */
-    public function getSwiftMessage(): \Swift_Message
+    public function getSwiftMessage(): \Swift_Mime_Message
     {
         return $this->swiftMessage;
     }
