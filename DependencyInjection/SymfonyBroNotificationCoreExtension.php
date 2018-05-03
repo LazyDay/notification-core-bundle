4c0828349f69dfcd8dc3f030e6e47b8186abe0b9
Artem <m@artemd.ru>
2018-05-03 23:54:58 +0300

    removed stolen driver configuration

--- DependencyInjection/SymfonyBroNotificationCoreExtension.php
+++ DependencyInjection/SymfonyBroNotificationCoreExtension.php
@@ -24,6 +24,5 @@ class SymfonyBroNotificationCoreExtension extends Extension
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
-        $loader->load('drivers.xml');
     }
 }
